a49b0a22cae0a0b8dcc189852062d517b5d7b87e
abel cortes <abelcortes1911@gmail.com>
2022-10-31 20:57:32 -0700

    log

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let score;
+score = 50
+console.log('score is:', score)
+score = 60
+console.log('score is:', score)
+
+let name = 'abel';
+let age = 21;
+
+// Camal notation (case)
+const interestRate = 2.5
+
+const InterestRate = 4.5
+
+// let name = 'John" // error
+
+age = 48
+
+//cannot reassign constant variable
+// interestRate = 3.5
+
+
+// Logging age to the console
+/*
+first line comment 
+another line 
+yet another line
+*/
+console.log(age)
+console.log(name)
+console.log(interestRate)
