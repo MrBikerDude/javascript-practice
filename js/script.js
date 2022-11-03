a9c4701756172a5a54d17574860b1899ba70d223
abel cortes <abelcortes1911@gmail.com>
2022-11-02 20:11:49 -0700

    daily

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-// outputs
-let name = 'abel';
-let favoritefood = 'pizza';
-
-
-
-//  console logs
-/*
-first line comment 
-another line 
-yet another line
-*/
-console.log(name)
-console.log(favoritefood)
-
-
+console.log("Supercalifragilisticexpialidocious".length);
+console.log(32)
+console.log(1234567890123456789012345678901234567890n)
